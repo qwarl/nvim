26d59668ecb0866dd5e02d9a6ea1a494523bb684
qwarl <quannngoc98@gmail.com>
2025-04-07 22:41:50 +0700

    treesitter: format code

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -115,40 +115,11 @@ return {
 			vim.keymap.set("n", "<leader>tc", "<cmd>TSContextToggle<CR>", { desc = "Toggle Treesitter Context" })
 		end,
 	},
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter-textobjects",
-	-- 	config = function()
-	-- 		require("nvim-treesitter.configs").setup({
-	-- 			textobjects = {
-	-- 				select = {
-	-- 					enable = true,
-	-- 					keymaps = {
-	-- 						["af"] = { query = "@function.outer", desc = "Select outer part of a function call" },
-	-- 						["if"] = { query = "@function.inner", desc = "Select inner part of a function call" },
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 		})
-	--
-	-- 		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-	--
-	-- 		-- vim way: ; goes to the direction you were moving.
-	-- 		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-	-- 		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-	--
-	-- 		-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-	-- 		vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-	-- 		vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-	-- 		vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-	-- 		vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-	-- 	end,
-	-- },
-
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		event = "VeryLazy",
-    -- enabled = false,
+		-- enabled = false,
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				textobjects = {
