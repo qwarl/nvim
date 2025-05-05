7aaf2844f0713cbf00d6b1c01267f4728cf12167
qwarl <quannngoc98@gmail.com>
2025-05-05 22:10:12 +0700

    refactor lsp (typescript)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,199 +1,315 @@
 local is_path = require("util")
 
 return {
-	-- Main LSP Configuration
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for Neovim
-		{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
+  -- Main LSP Configuration
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", config = true },
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    { "j-hui/fidget.nvim",       opts = {} },
+    "saghen/blink.cmp",
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+        end
 
-		-- Useful status updates for LSP.
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{ "j-hui/fidget.nvim", opts = {} },
+        -- get keymaps
+        local keys = require("util.lsp-keymaps").get()
 
-		-- Allows extra capabilities provided by nvim-cmp
-		"saghen/blink.cmp",
-	},
-	config = function()
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-			callback = function(event)
-				local map = function(keys, func, desc, mode)
-					mode = mode or "n"
-					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
-				end
+        for _, key in ipairs(keys) do
+          -- Kiểm tra `has` (nếu có)
+          if not key.has or require("util.lsp-keymaps").has(event.buf, key.has) then
+            -- Kiểm tra `cond` (nếu có)
+            if not key.cond or key.cond() then
+              local mode = key.mode or "n"
+              local opts = { buffer = event.buf, desc = key.desc }
+              if key.nowait then opts.nowait = true end
+              vim.keymap.set(mode, key[1], key[2], opts)
+            end
+          end
+        end
 
-				map("K", vim.lsp.buf.hover, "Show Hover Information")
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-				-- Rename the variable under your cursor.
-				--  Most Language Servers support renaming across files, etc.
-				map("<leader>rn", vim.lsp.buf.rename, "Rename")
+        -- Add vtsls special command
+        if client and client.name == "vtsls" then
+          -- keymaps for ts, js code
+          map("gD", function()
+            local params = vim.lsp.util.make_position_params()
+            vim.lsp.buf_request(0, "workspace/executeCommand", {
+              command = "typescript.goToSourceDefinition",
+              arguments = { params.textDocument.uri, params.position },
+            }, function(_, result)
+              if result then
+                vim.lsp.util.jump_to_location(result, "utf-8")
+              end
+            end)
+          end, "Goto Source Definition")
 
-				-- Execute a code action, usually your cursor needs to be on top of an error
-				-- or a suggestion from your LSP for this to activate.
-				map("<leader>ca", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+          -- Find All File References - TypeScript specific
+          map("gR", function()
+            vim.lsp.buf_request(0, "workspace/executeCommand", {
+              command = "typescript.findAllFileReferences",
+              arguments = { vim.uri_from_bufnr(0) },
+            }, function(_, result)
+              if result then
+                vim.lsp.util.locations_to_items(result, "utf-8")
+                require("snacks.picker").lsp_references()
+              end
+            end)
+          end, "File References")
 
-				-- WARN: This is not Goto Definition, this is Goto Declaration.
-				--  For example, in C this would take you to the header.
-				map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+          -- Organize Imports
+          map("<leader>co", function()
+            vim.lsp.buf.code_action({
+              apply = true,
+              context = {
+                only = { "source.organizeImports" },
+                diagnostics = {},
+              }
+            })
+          end, "Organize Imports")
 
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.document_highlight,
-					})
+          -- Add Missing Imports
+          map("<leader>cM", function()
+            vim.lsp.buf.code_action({
+              apply = true,
+              context = {
+                only = { "source.addMissingImports.ts" },
+                diagnostics = {},
+              }
+            })
+          end, "Add Missing Imports")
 
-					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.clear_references,
-					})
+          -- Remove Unused Imports
+          map("<leader>cu", function()
+            vim.lsp.buf.code_action({
+              apply = true,
+              context = {
+                only = { "source.removeUnused.ts" },
+                diagnostics = {},
+              }
+            })
+          end, "Remove Unused Imports")
 
-					vim.api.nvim_create_autocmd("LspDetach", {
-						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-						end,
-					})
-				end
+          -- Fix All Diagnostics
+          map("<leader>cD", function()
+            vim.lsp.buf.code_action({
+              apply = true,
+              context = {
+                only = { "source.fixAll.ts" },
+                diagnostics = {},
+              }
+            })
+          end, "Fix All Diagnostics")
 
-				if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-					-- Toggle inlay hints
-					map("<leader>uh", function()
-						local buf = vim.api.nvim_get_current_buf()
-						local enabled = not vim.lsp.inlay_hint.is_enabled({ bufnr = buf })
-						vim.lsp.inlay_hint.enable(enabled, { bufnr = buf })
+          -- Select TypeScript Version
+          map("<leader>cV", function()
+            vim.lsp.buf_request(0, "workspace/executeCommand", {
+              command = "typescript.selectTypeScriptVersion"
+            })
+          end, "Select TS Workspace Version")
 
-						-- Notify user
-						if enabled then
-							vim.notify("Inlay hints enabled for this buffer.", vim.log.levels.INFO)
-						else
-							vim.notify("Inlay hints disabled for this buffer.", vim.log.levels.INFO)
-						end
-					end, "Toggle Inlay Hints")
 
-					map("<leader>uH", function()
-						local enabled = not vim.lsp.inlay_hint.is_enabled()
-						vim.lsp.inlay_hint.enable(enabled)
+          client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+            local action, uri, range = unpack(command.arguments)
+            local function move(newf)
+              client.request("workspace/executeCommand", {
+                command = command.command,
+                arguments = { action, uri, range, newf },
+              })
+            end
+            local fname = vim.uri_to_fname(uri)
+            client.request("workspace/executeCommand", {
+              command = "typescript.tsserverRequest",
+              arguments = {
+                "getMoveToRefactoringFileSuggestions",
+                {
+                  file = fname,
+                  startLine = range.start.line + 1,
+                  startOffset = range.start.character + 1,
+                  endLine = range["end"].line + 1,
+                  endOffset = range["end"].character + 1,
+                },
+              },
+            }, function(_, result)
+              local files = result.body.files
+              table.insert(files, 1, "Enter new path...")
+              vim.ui.select(files, {
+                prompt = "Select move destination:",
+                format_item = function(f)
+                  return vim.fn.fnamemodify(f, ":~:.")
+                end,
+              }, function(f)
+                if f and f:find("^Enter new path") then
+                  vim.ui.input({
+                    prompt = "Enter move destination:",
+                    default = vim.fn.fnamemodify(fname, ":h") .. "/",
+                    completion = "file",
+                  }, function(newf)
+                    if newf then move(newf) end
+                  end)
+                elseif f then
+                  move(f)
+                end
+              end)
+            end)
+          end
+        end
 
-						-- Notify user
-						if enabled then
-							vim.notify("Inlay hints enabled globally.", vim.log.levels.INFO)
-						else
-							vim.notify("Inlay hints disabled globally.", vim.log.levels.INFO)
-						end
-					end, "Inlay Hints (Global)")
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
+          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
+          vim.api.nvim_create_autocmd("LspDetach", {
+            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+            end,
+          })
+        end
 
-					-- Enable inlay hints by default
-					-- vim.lsp.inlay_hint.enable()
-				end
-			end,
-		})
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+          map("<leader>uh", function()
+            local buf = vim.api.nvim_get_current_buf()
+            local enabled = not vim.lsp.inlay_hint.is_enabled({ bufnr = buf })
+            vim.lsp.inlay_hint.enable(enabled, { bufnr = buf })
+            if enabled then
+              vim.notify("Inlay hints enabled for this buffer.", vim.log.levels.INFO)
+            else
+              vim.notify("Inlay hints disabled for this buffer.", vim.log.levels.INFO)
+            end
+          end, "Toggle Inlay Hints")
 
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-		on_attach = function(client, bufnr)
-			if client.server_capabilities.inlayHintProvider then
-				vim.lsp.inlay_hint.enable(true, nil)
-			end
-		end
+          map("<leader>uH", function()
+            local enabled = not vim.lsp.inlay_hint.is_enabled()
+            vim.lsp.inlay_hint.enable(enabled)
+            if enabled then
+              vim.notify("Inlay hints enabled globally.", vim.log.levels.INFO)
+            else
+              vim.notify("Inlay hints disabled globally.", vim.log.levels.INFO)
+            end
+          end, "Inlay Hints (Global)")
+        end
+      end,
+    })
 
-		local servers = {
-			-- lsp
-			bashls = {
-				filetypes = { "sh", "zsh" },
-			},
-			texlab = {},
-			lua_ls = {
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-						hint = {
-							enable = true,
-							arrayIndex = "Disable",
-						},
-					},
-				},
-			},
-			html = {},
-			cssls = {},
-			tailwindcss = {},
-			emmet_ls = {},
-			-- gopls = {},
-			pyright = {},
-			rust_analyzer = {},
-			vtsls = {
-				settings = {
-					typescript = {
-						inlayHints = {
-							parameterNames = { enabled = "all" },
-							parameterTypes = { enabled = true },
-							variableTypes = { enabled = true },
-							propertyDeclarationTypes = { enabled = true },
-							functionLikeReturnTypes = { enabled = true },
-							enumMemberValues = { enabled = true },
-						},
-					},
-				},
-			},
-			marksman = {},
-			jsonls = {
-				-- lazy-load schemastore when needed
-				on_new_config = function(new_config)
-					new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-					vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-				end,
-				settings = {
-					json = {
-						format = {
-							enable = true,
-						},
-						validate = { enable = true },
-					},
-				},
-			},
-			-- sqls = {},
-			taplo = {},
-			yamlls = {},
-		}
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+    capabilities.workspace = capabilities.workspace or {}
+    capabilities.workspace.fileOperations = {
+      willRename = true,
+      didRename = true,
+    }
 
-		if is_path.exists_in_config("hypr") then
-			servers.hyprls = {}
-		end
+    local servers = {
+      bashls = { filetypes = { "sh", "zsh" } },
+      texlab = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            completion = { callSnippet = "Replace" },
+            hint = {
+              enable = true,
+              arrayIndex = "Disable",
+            },
+          },
+        },
+      },
+      html = {},
+      cssls = {},
+      tailwindcss = {},
+      emmet_ls = {},
+      pyright = {},
+      rust_analyzer = {},
+      vtsls = {
+        settings = {
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "all" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = true },
+            },
+          },
+        },
+      },
+      marksman = {},
+      jsonls = {
+        on_new_config = function(new_config)
+          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+          vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+        end,
+        settings = {
+          json = {
+            format = { enable = true },
+            validate = { enable = true },
+          },
+        },
+      },
+      taplo = {},
+      yamlls = {},
+    }
 
-		require("mason").setup()
+    if is_path.exists_in_config("hypr") then
+      servers.hyprls = {}
+    end
 
-		local ensure_installed = vim.tbl_keys(servers or {})
-		vim.list_extend(ensure_installed, {
-			-- linter and formatter
-			"black",
-			"eslint_d",
-			"markdownlint-cli2",
-			"markdown-toc",
-			"prettier",
-			"pylint",
-			"shellcheck",
-			"shfmt",
-			"stylua", -- Used to format Lua code
-		})
-		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			handlers = {
-				function(server_name)
-					local server = servers[server_name] or {}
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
-			},
-		})
-	end,
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, {
+      "black",
+      "eslint_d",
+      "markdownlint-cli2",
+      "markdown-toc",
+      "prettier",
+      "pylint",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+    })
+    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+    require("mason-lspconfig").setup({
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+          require("lspconfig")[server_name].setup(server)
+        end,
+      },
+    })
+  end,
 }
---
+++ lua/util/lsp-keymaps.lua
@@ -0,0 +1,102 @@
+local M = {}
+
+M._keys = nil
+
+function M.get()
+  if M._keys then
+    return M._keys
+  end
+  -- stylua: ignore
+  M._keys = {
+    { "<leader>cl", function() Snacks.picker.lsp_config() end,          desc = "Lsp Info" },
+    { "gd",         vim.lsp.buf.definition,                             desc = "Goto Definition",            has = "definition" },
+    { "gr",         vim.lsp.buf.references,                             desc = "References",                 nowait = true },
+    { "gI",         vim.lsp.buf.implementation,                         desc = "Goto Implementation" },
+    { "gy",         vim.lsp.buf.type_definition,                        desc = "Goto T[y]pe Definition" },
+    { "gD",         vim.lsp.buf.declaration,                            desc = "Goto Declaration" },
+    { "K",          function() return vim.lsp.buf.hover() end,          desc = "Show Hover Information" },
+    { "gK",         function() return vim.lsp.buf.signature_help() end, desc = "Signature Help",             has = "signatureHelp" },
+    { "<c-k>",      function() return vim.lsp.buf.signature_help() end, mode = "i",                          desc = "Signature Help", has = "signatureHelp" },
+    { "<leader>ca", vim.lsp.buf.code_action,                            desc = "Code Action",                mode = { "n", "v" },     has = "codeAction" },
+    { "<leader>cc", vim.lsp.codelens.run,                               desc = "Run Codelens",               mode = { "n", "v" },     has = "codeLens" },
+    { "<leader>cC", vim.lsp.codelens.refresh,                           desc = "Refresh & Display Codelens", mode = { "n" },          has = "codeLens" },
+    { "<leader>cR", function() Snacks.rename.rename_file() end,         desc = "Rename File",                mode = { "n" },          has = { "workspace/didRenameFiles", "workspace/willRenameFiles" } },
+    { "<leader>rn", vim.lsp.buf.rename,                                 desc = "Rename",                     has = "rename" },
+    {
+      "<leader>cA",
+      function()
+        vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {}, }, })
+      end,
+      desc = "Source Action",
+      has = "codeAction"
+    },
+    {
+      "]]",
+      function() Snacks.words.jump(vim.v.count1) end,
+      has = "documentHighlight",
+      desc = "Next Reference",
+      cond = function() return Snacks.words.is_enabled() end
+    },
+    {
+      "[[",
+      function() Snacks.words.jump(-vim.v.count1) end,
+      has = "documentHighlight",
+      desc = "Prev Reference",
+      cond = function() return Snacks.words.is_enabled() end
+    },
+    {
+      "<a-n>",
+      function() Snacks.words.jump(vim.v.count1, true) end,
+      has = "documentHighlight",
+      desc = "Next Reference",
+      cond = function() return Snacks.words.is_enabled() end
+    },
+    {
+      "<a-p>",
+      function() Snacks.words.jump(-vim.v.count1, true) end,
+      has = "documentHighlight",
+      desc = "Prev Reference",
+      cond = function() return Snacks.words.is_enabled() end
+    },
+  }
+
+  return M._keys
+end
+
+function M.get_clients(opts)
+  local ret = {} ---@type vim.lsp.Client[]
+  if vim.lsp.get_clients then
+    ret = vim.lsp.get_clients(opts)
+  else
+    ---@diagnostic disable-next-line: deprecated
+    ret = vim.lsp.get_active_clients(opts)
+    if opts and opts.method then
+      ---@param client vim.lsp.Client
+      ret = vim.tbl_filter(function(client)
+        return client.supports_method(opts.method, { bufnr = opts.bufnr })
+      end, ret)
+    end
+  end
+  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
+function M.has(buffer, method)
+  if type(method) == "table" then
+    for _, m in ipairs(method) do
+      if M.has(buffer, m) then
+        return true
+      end
+    end
+    return false
+  end
+  method = method:find("/") and method or "textDocument/" .. method
+  local clients = M.get_clients({ bufnr = buffer })
+  for _, client in ipairs(clients) do
+    if client.supports_method(method) then
+      return true
+    end
+  end
+  return false
+end
+
+return M
